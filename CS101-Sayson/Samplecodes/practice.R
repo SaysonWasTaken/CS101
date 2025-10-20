dd2d0a7b4d3ab660776003cf0c0d143ecf7fac4e
Adrian Sayson <adrian.sayson@students.isatu.edu.ph>
2025-10-20 00:36:25 +0000

    commit

---
+++ CS101-Sayson/Samplecodes/practice.R
@@ -0,0 +1,13 @@
+brands <- c("Toyota", "Nissan", "Mitsubishi", "Honda")
+toyota <- c("Supra", "MR2", "Celica")
+nissan <- c("Skyline", "350z", "Silvia")
+mitsubishi <- c("Lancer", "Eclipse", "GT3000")
+
+cars <- data.frame(
+  Brands = brands,
+  Toyota = toyota,
+  Nissan = nissan,
+  Mitsubishi = mitsubishi
+)
+
+cars
